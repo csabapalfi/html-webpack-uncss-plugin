95e930bcf125c289bfb9d726a02c0f5f489636fa
Csaba Palfi <csaba@palfi.me>
2016-09-24 01:23:02 +0100

    formatting

--- index.js
+++ index.js
@@ -4,31 +4,33 @@ const cheerio = require('cheerio');
 const uncss = require('uncss');
 
 function HtmlWebpackUncssPlugin (options) {
-  assert.equal(options, undefined, 'The HtmlWebpackUncssPlugin does not accept any options');
+  assert.equal(options, undefined,
+    'The HtmlWebpackUncssPlugin does not accept any options');
 }
 
 HtmlWebpackUncssPlugin.prototype.apply = (compiler) => {
-  compiler.plugin('compilation', function(compilation) {
-    compilation.plugin('html-webpack-plugin-after-html-processing', function(htmlPluginData, callback) {
-      const $ = cheerio.load(htmlPluginData.html);
-      const styles = [];
-      $('style').each(function() {
-        const style = $(this).html();
-        if (style) {
-          styles.push(style);
-        }
-      });
+  compiler.plugin('compilation', (compilation) => {
+    compilation.plugin('html-webpack-plugin-after-html-processing',
+      (data, callback) => {
+        const $ = cheerio.load(data.html);
+        const styles = [];
+        $('style').each(function() {
+          const style = $(this).html();
+          if (style) {
+            styles.push(style);
+          }
+        });
 
-      uncss($.html(), { raw: styles.join(' ') }, (error, output) => {
-        if (error) {
-          return callback(error);
-        }
-        $('style').slice(1).remove();
-        $('style').text(output);
-        htmlPluginData.html = $.html();
-        return callback(null, htmlPluginData);
+        uncss($.html(), { raw: styles.join(' ') }, (error, output) => {
+          if (error) {
+            return callback(error);
+          }
+          $('style').slice(1).remove();
+          $('style').text(output);
+          data.html = $.html();
+          return callback(null, data);
+        });
       });
-    });
   });
 };
 
